df22f699dccd54269fb794a309bc9b899dd0822f
REGENT EDUCATION & RESEARCH FOUNDATION <41721646+rerfcse123@users.noreply.github.com>
2018-07-26 14:09:59 +0530

    Update Program to Find Transpose of a Matrix.c

--- Program to Find Transpose of a Matrix.c
+++ Program to Find Transpose of a Matrix.c
@@ -1,14 +1,10 @@
 //Program to Find Transpose of a Matrix
 #include <stdio.h>
-
-
 int main()
 {
-    int a[10][10], transpose[10][10], r, c, i, j;
-    printf("Enter rows and columns of matrix: ");
-    scanf("%d %d", &r, &c);
-
-    // Storing elements of the matrix
+  int a[10][10], transpose[10][10], r, c, i, j;
+   printf("Enter rows and columns of matrix: ");
+  scanf("%d %d", &r, &c);
     printf("\nEnter elements of matrix:\n");
     for(i=0; i<r; ++i)
         for(j=0; j<c; ++j)
@@ -16,8 +12,6 @@ int main()
             printf("Enter element a%d%d: ",i+1, j+1);
             scanf("%d", &a[i][j]);
         }
-
-    // Displaying the matrix a[][] */
     printf("\nEntered Matrix: \n");
     for(i=0; i<r; ++i)
         for(j=0; j<c; ++j)
@@ -27,14 +21,11 @@ int main()
                 printf("\n\n");
         }
 
-    // Finding the transpose of matrix a
     for(i=0; i<r; ++i)
         for(j=0; j<c; ++j)
         {
             transpose[j][i] = a[i][j];
         }
-
-    // Displaying the transpose of matrix a
     printf("\nTranspose of Matrix:\n");
     for(i=0; i<c; ++i)
         for(j=0; j<r; ++j)
