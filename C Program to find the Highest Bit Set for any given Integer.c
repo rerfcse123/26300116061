ef61361e2192676bf758c6649894e81b567a1558
REGENT EDUCATION & RESEARCH FOUNDATION <41721646+rerfcse123@users.noreply.github.com>
2018-07-26 14:08:25 +0530

    Update C Program to find the Highest Bit Set for any given Integer.c

--- C Program to find the Highest Bit Set for any given Integer.c
+++ C Program to find the Highest Bit Set for any given Integer.c
@@ -2,23 +2,18 @@
  
 #include <stdio.h>
 #define NUM_BITS sizeof(int)*8
- 
-int highest_bit_set(int);
+ int highest_bit_set(int);
 void display(int);
 int i = NUM_BITS;
- 
-void main()
+ void main()
 {
-    int num, pos;
- 
-    printf("\nenter the number:");
-    scanf("%d", &num);
- 
-    display(num);
-    pos = highest_bit_set(num);
-    printf("\nthe position of the highest bit set is %d", pos);
+ int num, pos;
+printf("\nenter the number:");
+ scanf("%d", &num);
+display(num);
+ pos = highest_bit_set(num);
+ printf("\nthe position of the highest bit set is %d", pos);
 }
-/* RETURNS THE POSITION */
 int highest_bit_set(int num)
 {
     int count = 0;
@@ -29,7 +24,6 @@ int highest_bit_set(int num)
     }
     return(count);
 }
-/* DISPLAYS THE NUMBER IN BINARY REPRESENTATION */
 void display(int num)
 {
     int c;
